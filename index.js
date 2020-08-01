2d02449e65206df79abe357113b06a35c6930374
JeremyWells227 <jswe227@gmail.com>
2020-07-31 23:45:17 -0400

    Custom sizing working

--- index.js
+++ index.js
@@ -14,26 +14,51 @@ const resetGrid = ()=> {
 	})
 }
 
+const cleanupOldGrid = ()=>{
+	let grid = document.getElementById("grid");
+	let existingCells = Array.from(document.getElementsByClassName("cell"));
+	existingCells.forEach((cell)=>{
+		grid.removeChild(cell)
+	}
+	)
+}
+
+let initialGrid = false
 const createGrid = ()=>{
-	const grid = document.getElementById("grid")
+	if(initialGrid){
+		cleanupOldGrid();
+	}
+
+	const grid = document.getElementById("grid");
 	console.log("Creating Grid...");
-	const width=grid.clientWidth;
-	const height=grid.clientHeight;
+	const widthStr=document.getElementById("canvassize").value;
+	const boxsideStr=document.getElementById("boxsize").value;
+	const width = parseInt(widthStr);
+	const boxside = parseInt(boxsideStr);
+	if(boxside < 5) {
+		alert("Box size is too small!  Minimum 5 px for speed")
+		return
+	}
+
+	const height=width;
+	grid.style["width"] = widthStr+'px';
+	grid.style["height"] = widthStr+'px';
+	console.log(grid.style);
+	
 	const area = width*height;
-	const boxside = 10;
-	console.log(width);
-	console.log(height);
+  console.log("Creating Grid...");
 	for(let i=0; i < area/(boxside*boxside); i+=1){
-		console.log("Creating Grid...");
 		let newCell = document.createElement("div");
-		newCell.style.width=boxside+'px'
-		newCell.style.height=boxside+'px'
+		newCell.style['width']=boxside+'px'
+		newCell.style['height']=boxside+'px'
 		newCell.classList.add("cell");
 		newCell.id=`cell${i}`;
 		newCell.addEventListener("mouseover",markBlack)
 		grid.appendChild(newCell)
 	}
+	console.log("Done!")
+
+	initialGrid=true;
 	
 }
-
-window.onload = createGrid;
+window.onload = createGrid()
